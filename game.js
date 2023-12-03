62cf8107a0aad480c56d4cacf9907b76b2993051
V.Sahanashre <sahanashre.v@kalvium.community>
2023-12-03 19:07:01 +0530

    Added

--- game.js
+++ game.js
@@ -3,3 +3,77 @@
 // Iteration 3: Make the options button functional
 
 // Iteration 4: Build a timer for the game
+let referenceTime=undefined;
+let number1 = document.getElementById("number1");
+let number2 = document.getElementById("number2");
+let score = 0;
+let Timer = document.getElementById("timer");
+ResetTime();
+let num1=Math.floor(Math.random()*100)
+let num2=Math.floor(Math.random()*100)
+number1.innerText=num1;
+number2.innerText=num2;
+let greaterBtn = document.getElementById("greater-than");
+greaterBtn.addEventListener("click",function()
+{
+    if (num1>num2){
+        score+=1
+        num1=Math.floor(Math.random()*100)
+        num2=Math.floor(Math.random()*100)
+        number1.innerText=num1;
+        number2.innerText=num2;
+        clearInterval(referenceTime);
+        ResetTime();
+    }
+    else{
+        localStorage.setItem("score",score);
+        window.location.href='./gameover.html';
+    }
+})
+let lesserBtn = document.getElementById("lesser-than");
+lesserBtn.addEventListener("click",function(){
+    if (num1<num2){
+        score+=1
+        num1=Math.floor(Math.random()*100)
+        num2=Math.floor(Math.random()*100)
+        number1.innerText=num1;
+        number2.innerText=num2;
+        clearInterval(referenceTime);
+        ResetTime();
+    }
+    else{
+        localStorage.setItem("score",score)
+        window.location.href="./gameover.html";
+    }
+
+})
+
+let equalBtn = document.getElementById("equal-to");
+equalBtn.addEventListener("click",function(){
+    if(num1==num2){
+        score+=1
+        num1=Math.floor(Math.random()*100)
+        num2=Math.floor(Math.random()*100)
+        number1.innerText=num1;
+        number2.innerText=num2;
+        clearInterval(referenceTime);
+        ResetTime();
+    }
+    else{
+        localStorage.setItem("score",score)
+        window.location.href="./gameover.html";
+    }
+})
+
+function ResetTime(){
+    let Time=5;
+    Timer.innerText = Time;
+    referenceTime=setInterval(() =>{
+        Time-=1
+        Timer.innerText=Time;
+        if(Time==0){
+            localStorage.setItem("score",score);
+            window.location.href="./gameover.html";
+        }
+    },1000);
+}
--- score.js
+++ score.js
@@ -1,1 +1,8 @@
 // Iteration 5: Store the player score and display it on the game over screen
+let scoreGetFromStorage = localStorage.getItem("score");
+let scoreBoard = document.getElementById("score-board");
+scoreBoard.innerText = scoreGetFromStorage;
+let playAgain = document.getElementById("play-again-button");
+playAgain.addEventListener("click",function(){
+    window.location.href="./index.html";
+})
